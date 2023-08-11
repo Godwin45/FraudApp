d1451145fe2875ccd6a8d5322ea1767f9253d018
Godwin45 <godwinmuthomi12@gmail.com>
2023-08-11 23:13:10 +0300

    data transformation complete

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_cleaning import DataCleaningPipeline
 from mlProject.pipeline.stage_03_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_04_data_transformation import DataTransformationTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -33,4 +35,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 
